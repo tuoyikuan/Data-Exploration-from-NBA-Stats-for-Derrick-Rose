7a28ac2260d252d5d58975ab90ea22a5ffac70e7
Tuoyikuan <3170105665@zju.edu.cn>
2019-09-20 18:42:39 +0800

    parse url

--- Spider/Spider/spiders/nbastats.py
+++ Spider/Spider/spiders/nbastats.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from urllib.parse import urljoin, urlencode
 
 class NbastatsSpider(scrapy.Spider):
     name = 'nbastats'
     allowed_domains = ['stats.nba.com']
-    start_urls = ['http://stats.nba.com/']
+    base = 'http://stats.nba.com/player/'
+    player_id = '201565/'
+    data_type = 'shots-dash/'
+    years = range(16, 18)
+    url = urljoin(base, player_id+data_type)
+    start_urls = []
+    for year in years:
+        parameters = {
+            'Season' : '20'+str(year)+'-'+str(year+1),
+            'SeasonType' : 'Regular Seanson'
+        }
+        start_urls.append(urljoin(url,'?'+urlencode(parameters)))
+    # print(start_urls)
+
+
 
     def parse(self, response):
-        pass
+
